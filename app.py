f8ade76b125e0c8a8521c0910da038461aec0788
persynl24 <persyn.l@gmail.com>
2025-12-04 09:34:59 +0100

    change subject of mail

--- app.py
+++ app.py
@@ -209,7 +209,7 @@ Your IOC/BV
 """
 
             eml_content = (
-                f"Subject: PRIAM Special Permission about to expire - {datetime.now().strftime('%Y-%m-%d')}\n"
+                f"Subject: Special Permissions to be reviewed for your team"
                 f"Content-Type: text/html; charset=UTF-8\n"
                 f"\n"
                 f"{email_body}"
